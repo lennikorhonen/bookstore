40d5de70b20e34ab2142ad3ce6bf4362d8e217de
Lenni Korhonen <lenni.korhonen2@gmail.com>
2020-03-12 18:35:44 +0200

    Added user entities istead of in-memory users

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -1,5 +1,7 @@
 package com.example.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,16 +11,20 @@ import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
 import com.example.bookstore.domain.Category;
 import com.example.bookstore.domain.CategoryRepository;
+import com.example.bookstore.domain.User;
+import com.example.bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 	
 	@Bean
-	public CommandLineRunner exampleBook(BookRepository brepository, CategoryRepository crepository) {
+	public CommandLineRunner exampleBook(BookRepository brepository, CategoryRepository crepository, UserRepository urepository) {
 		return(args) ->{
 			crepository.save(new Category("Fiktio"));
 			crepository.save(new Category("Historia"));
@@ -26,6 +32,16 @@ public class BookstoreApplication {
 			
 			brepository.save(new Book("Fahrenheit 451", "Ray Bradbury", 1953, 978880427, 25, crepository.findByName("Fiktio").get(0)));
 			brepository.save(new Book("Pääteasema Auschwitz", "Eddy de Wind", 2020, 978880451, 30, crepository.findByName("Historia").get(0)));
+			
+			User user1 = new User("user", "$2y$12$SusvugFmsbBgGdkylPfAYeRGkEdZU0wk39Nh71fPndQt1MNJMXCIi", "USER");
+			User user2 = new User("admin", "$2y$12$oCn6VjnDtbnh73JtE.9O9e3t4xPV.9gJdt8rj5YuJIA5TZEYI7moO", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
+			
+			log.info("Fetch all books");
+			for (Book book : brepository.findAll()) {
+				log.info(book.toString());
+			}
 		};
 	}
 
--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -53,7 +53,7 @@ public class BookController {
 		return "addbook";
 	}
 	
-	@PreAuthorize("hasRole('ADMIN')")
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		repository.deleteById(bookId);
