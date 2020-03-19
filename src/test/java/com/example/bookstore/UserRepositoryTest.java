93be9c4e25097e5cd6a029fcba53f2ba0025f3c5
Lenni Korhonen <lenni.korhonen2@gmail.com>
2020-03-19 22:10:36 +0200

    Small change

--- src/test/java/com/example/bookstore/UserRepositoryTest.java
+++ src/test/java/com/example/bookstore/UserRepositoryTest.java
@@ -3,7 +3,6 @@ package com.example.bookstore;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
