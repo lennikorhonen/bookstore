7062c1926ee61c79d906fb24dd79fa138dfc768a
Lenni Korhonen <lenni.korhonen2@gmail.com>
2020-03-19 22:07:52 +0200

    Small change

--- src/test/java/com/example/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/example/bookstore/BookstoreApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.bookstore;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
--- src/test/java/com/example/bookstore/CatRepositoryTest.java
+++ src/test/java/com/example/bookstore/CatRepositoryTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
