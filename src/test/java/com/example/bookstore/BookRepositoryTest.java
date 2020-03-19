b0254cd9530b750ffc06cee756e0ef9e836e30e8
Lenni Korhonen <lenni.korhonen2@gmail.com>
2020-03-19 21:59:31 +0200

    Test files for Controller and repositories

---
+++ src/test/java/com/example/bookstore/BookRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.example.bookstore;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.bookstore.domain.Book;
+import com.example.bookstore.domain.BookRepository;
+import com.example.bookstore.domain.Category;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class BookRepositoryTest {
+	
+	@Autowired
+	private BookRepository repository;
+
+	@Test
+	public void findByTitleShouldReturnBook() {
+		List<Book> books = repository.findByTitle("Fahrenheit 451");
+		assertThat(books).hasSize(1);
+		assertThat(books.get(0).getAuthor()).isEqualTo("Ray Bradbury");
+	}
+	
+	@Test
+	public void createNewBook() {
+		Book book = new Book("Nakki", "Vene", 1999, 123456, 30, new Category("Seikkailu"));
+		repository.save(book);
+		assertThat(book.getId()).isNotNull();
+	}
+
+}
---
+++ src/test/java/com/example/bookstore/CatRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.example.bookstore;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.bookstore.domain.CategoryRepository;
+import com.example.bookstore.domain.Category;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class CatRepositoryTest {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	@Test
+	public void findByTitleShouldReturnCategory() {
+		List<Category> category = repository.findByName("Fiktio");
+		assertThat(category).hasSize(1);
+		assertThat(category.get(0).getBooks()).allMatch(null, "Fahrenheit 451");
+	}
+	
+	@Test
+	public void createNewCategory() {
+		Category category = new Category("Seikkailu");
+		repository.save(category);
+		assertThat(category.getId()).isNotNull();
+	}
+
+}
